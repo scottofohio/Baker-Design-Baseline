7b8307e9bed25f0806356649e16b7c00d345cce5
Scott <scottbakers@gmail.com>
2019-11-11 09:15:13 -0500

    wp core

--- index.php
+++ index.php
@@ -1,32 +1,17 @@
 <?php
 /**
- * The main template file
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * This is the most generic template file in a WordPress theme and one
- * of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query,
- * e.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Baker Design Theme 1.0
- * @subpackage theme2
+ * @package WordPress
  */
 
-get_header(); ?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-<?php while ( have_posts() ) : the_post(); ?>
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <section>
-        	<h2 class="post-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-	        <?php the_excerpt(); ?>
-	        <a href="<?php the_permalink(); ?>">Read More</a>
-        </section>
-    </article><!-- #post-<?php the_ID(); ?> -->
-<?php endwhile; ?>
-<?php echo my_post_nav_links(); ?>
-<?php get_sidebar(); ?>
-
-
-<?php
-get_footer();
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
