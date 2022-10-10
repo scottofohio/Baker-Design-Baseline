797084e6a39768ca5c5926e10cd9e545d5d385b6
Scott Baker <scott@bakerdesign.co>
2022-10-10 10:16:22 -0400

    switch to sass from scss and added gulp

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const { series, parallel, watch, task, src, dest } = require('gulp');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const sass = require('sass');
+const fs = require('fs');
+
+
+const clean = (cb) => {
+  console.log('running task');
+  cb();
+}
+
+
+const sassCompile = (cb) => {
+    const file = 'assets/sass/app.sass';
+    const result = sass.compile(file, {style: "compressed", sourceMap: true} );    
+    fs.writeFile('assets/css/app.css', result.css, function (err) {
+      if (err) return console.log(err);
+    });
+    console.log(result)
+
+    // fs.writeFile('assets/css/app.css.map', result.map, function (err) {
+    //   if (err) return console.log(err);
+    // });
+
+  cb()
+}
+
+
+
+const javascript = () => {
+     return src(['assets/js/custom/*.js'])
+  .pipe(concat('assets/js/app.js'))
+  .pipe(uglify())
+  .pipe(rename({suffix: '.min'}))
+  .pipe(dest('./'));
+
+}
+
+task('build', function(cb) {
+  // body omitted
+  watch([
+    'assets/sass/*.sass', 
+    'assets/sass/*/*.sass', 
+    'assets/js/**/*.js'], 
+    series(clean, parallel(javascript, sassCompile)));
+    cb();
+});
